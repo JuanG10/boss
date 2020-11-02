bdb9d2fd13668bacc85eaebbdb16a160e660dd30
JuanG10 <juangaray463@gmail.com>
2020-11-02 00:58:48 -0300

    miniRefactor

--- UpgradeScreen/UpgradeWindow.gd
+++ UpgradeScreen/UpgradeWindow.gd
@@ -15,19 +15,16 @@ onready var panel_matrix = [LEVELS.color_level,LEVELS.base_level] # y
 var actualPanel:Panel
 var actual_pos = Vector2(0,1) # Cuidado, 'y' es el segundo argumento
 
-onready var transition:TextureProgress = $TransitionAnimation
 signal start_animation
 
 func _ready():
-	actualPanel = panel_matrix[actual_pos.y][actual_pos.x].instance()
-	actualPanel.show_behind_parent = true
 	_add_actual_panel()
 
 func change_panel_to(fill_mode:int, next_panel:Vector2)->void:
 	actual_pos += next_panel
 	if _array_problems(): actual_pos -= next_panel
 	else:
-		transition.set_fill_mode(fill_mode)
+		$TransitionAnimation.set_fill_mode(fill_mode)
 		emit_signal("start_animation")
 
 func _array_problems()->bool:
@@ -35,10 +32,10 @@ func _array_problems()->bool:
 
 func _on_end_animation():
 	actualPanel.queue_free()
-	actualPanel = panel_matrix[actual_pos.y][actual_pos.x].instance()
-	actualPanel.show_behind_parent = true
 	_add_actual_panel()
 	emit_signal("start_animation")
 
 func _add_actual_panel()->void:
+	actualPanel = panel_matrix[actual_pos.y][actual_pos.x].instance()
+	actualPanel.show_behind_parent = true
 	get_node("/root/UpgradeWindow").add_child(actualPanel)
