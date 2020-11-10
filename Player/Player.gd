1f27755f8bb54147e5ef4b0422f0730d11e91b6d
Alejandro214 <alejandromamanijatabe@gmail.com>
2020-11-10 00:59:17 -0300

    botones de habilidades

--- Player/Player.gd
+++ Player/Player.gd
@@ -31,12 +31,14 @@ onready var G = get_node("States/Green")
 onready var B = get_node("States/Blue")
 
 
-
+#Muchos variables true, sacar en el refactor
 var habilidad_health 	= true 
 var habilidad_speed  	= true
 var habilidad_atk_speed = true 
 var habilidad_damage    = true
 var habilidad_corazon   = true
+var habilidad_damage_1  = true
+var habilidad_cuchillo  = true 
 
 func initialize(l,c):
 	label = l
@@ -64,6 +66,8 @@ func set_habilidades():
 	$States.get_node("Red").habilidades_actual = GlobalVariables.habilidades_Red
 	$States.get_node("Blue").set_habilidades(GlobalVariables.habilidades_Blue)
 	$States.get_node("Green").habilidades_actual = GlobalVariables.habilidades_Green
+
+
 
 func get_habiladades_de_state(state):
 	return $States.get_node(state).habilidades_actual	
@@ -123,46 +127,55 @@ func _physics_process(_delta):
 		position.y += speed
 	if Input.is_action_pressed('up'):
 		position.y -= speed
-	
 	#Si tiene la habiladad la usa	
-	if Input.is_action_just_pressed("Activate_shield") && existe_habilidad("shield") && habilidad_health && color_actual() == "Verde":
+	##Listo
+	if Input.is_action_just_pressed("Activate_shield") && existe_habilidad("shield") && habilidad_health && color_actual() == "Green":
 		efecto_de_health()
-		activar_timer()	
+		activar_Timer_shield()	
+	##Listo	
 	if Input.is_action_just_released("Activate_corazon") && existe_habilidad("Corazon") && habilidad_corazon && color_actual() == "Blue":
 		efecto_de_corazon()
-		activar_timer()
-			
-	if Input.is_action_just_pressed("Activate_speed") && existe_habilidad("speed") && habilidad_speed:
-		efecto_de_speed()
-		activar_timer()
-	if Input.is_action_just_pressed("Activate_damage") && existe_habilidad("damage") && habilidad_damage:
-		efecto_de_damage()
-		activar_timer()
-	if Input.is_action_just_pressed("Activate_atk_speed") && existe_habilidad("atk_speed") && habilidad_atk_speed:
-		efecto_de_atk_speed()
-		activar_timer()	
+		activar_Timer_Corazon()
 		
+	#Boton 2
+	if Input.is_action_just_pressed("Activate_speed") && existe_habilidad("Speed") && habilidad_speed:
+			efecto_de_speed()
+			activar_Timer_speed()
+			
+	#Boton 1
+	if Input.is_action_just_pressed("Activate_damage") && existe_habilidad("damage") && habilidad_damage:
+			efecto_de_damage()
+			activar_Timer_damage()
+	#Boton 3		
+	if Input.is_action_just_pressed("Activate_atk_speed") && existe_habilidad("atk_speed") && habilidad_atk_speed:
+			#Bala
+			efecto_de_atk_speed()
+			activar_Timer_atk_speed()	
+   #Boton 4
+	if Input.is_action_just_pressed("Activate_damage_1") && existe_habilidad("damage_1") && habilidad_damage_1 :
+			efecto_de_damage_1()
+			activar_Timer_damage_1()	
+	#Boton 5		
+	if Input.is_action_just_pressed("Activate_cuchillo") && existe_habilidad("Cuchillo") && habilidad_cuchillo :
+		efecto_de_cuchillo()
+		activar_Timer_cuchillo()
+		print(5)
+			
+		
+func efecto_de_cuchillo():
+	GlobalVariables.dmg += 9
+	dmg = GlobalVariables.dmg
+	habilidad_cuchillo = false
+	
+func efecto_de_damage_1():
+	GlobalVariables.dmg += 5
+	dmg = GlobalVariables.dmg
+	habilidad_damage_1 = false
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+func activar_Timer_Corazon():
+	$Timer_corazon.set_wait_time(5)
+	$Timer_corazon.start()
+	
 
 
 func efecto_de_corazon():
@@ -198,15 +211,15 @@ func efecto_de_atk_speed():
 	habilidad_atk_speed = false
 	
 	
-func activar_timer():
-	$Timer.set_wait_time(5)
-	$Timer.start()
+func activar_Timer_shield():
+	$Timer_shield.set_wait_time(5)
+	$Timer_shield.start()
 		
 
 
 func existe_habilidad(nombre_habilidad):
 	var boolean = false 
-	for name_habilidad in GlobalVariables.habilidadres:
+	for name_habilidad in habilidades_player():
 		boolean = nombre_habilidad == name_habilidad
 	return boolean	
 
@@ -255,28 +268,99 @@ func _on_grab_coin(area):
 
 func active_health():
 	 habilidad_health = true 
-	 $Timer.stop()
+	 $Timer_shield.stop()
 
-func active_speed():
-	habilidad_speed = true
-	$Timer.stop()
 	
-func active_atk_speed():
-	habilidad_atk_speed = true 
-	$Timer.stop()
-	
-func active_damage():
-	habilidad_damage = true
-	$Timer.stop()			
+
+				
 
 func _on_Timer_timeout():
 	if !habilidad_health:
 		active_health() 
 
 
+#Timer de la habilidad de corazon
+func _on_Timer_corazon_timeout():
+	if !habilidad_corazon:
+		active_corazon()
+
+func active_corazon():
+	habilidad_corazon = true 
+	$Timer_corazon.stop()
+	
+	
+#Timer de la habilidad de speed 
+func activar_Timer_speed():
+	$Timer_Speed.set_wait_time(5)
+	$Timer_Speed.start()
+		
+
+func active_speed():
+	habilidad_speed = true
+	$Timer_Speed.stop()
+	
+		
+func _on_Timer_Speed_timeout():
+	active_speed()
 
 
 
 
 
+#Timer damage
+func activar_Timer_damage():
+	$Timer_damage.set_wait_time(5)
+	$Timer_damage.start()
 
+func active_damage():
+	habilidad_damage = true
+	$Timer_damage.stop()	
+	
+	
+func _on_Timer_damage_timeout():
+	active_damage()
+	
+	
+	
+	
+#Timer atk speed
+func activar_Timer_atk_speed():
+	$Timer_atk_speed.set_wait_time(5)
+	$Timer_atk_speed.start()
+	
+func active_atk_speed():
+	habilidad_atk_speed = true
+	$Timer_atk_speed.stop()	
+
+func _on_Timer_atk_speed_timeout():
+	active_atk_speed()
+		
+
+#Timer damage_1
+func activar_Timer_damage_1():
+	$Timer_damage_1.set_wait_time(5)
+	$Timer_damage_1.start()
+	
+		 # Replace with function body.
+func active_damage_1():
+	habilidad_damage_1 = true 
+	$Timer_damage_1.stop()
+
+
+func _on_Timer_damage_1_timeout():
+	activar_Timer_damage_1()
+
+
+#Timer cuchillo 
+func activar_Timer_cuchillo():
+	$Timer_cuchillo.set_wait_time(5)
+	$Timer_cuchillo.start()
+	
+func active_cuchillo():
+	habilidad_cuchillo = true
+	$Timer_cuchillo.stop()
+	
+		
+
+func _on_Timer_cuchillo_timeout():
+	active_cuchillo()
